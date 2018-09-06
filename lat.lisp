99af89b706c4c25d39f6134f609c33f7cb071e36
Jerred Shepherd <ShepherdJerred@gmail.com>
2018-09-06 15:18:53 -0500

    Impl

--- lat.lisp
+++ lat.lisp
@@ -1,2 +1,18 @@
-(+ 5 6)
+(defun lat? (x)
+  (if (listp x)
+    (if (= (list-length x) '0)
+      't
+      (if (atom (first x))
+        (lat? (cdr x))
+        'nil
+      )
+    )
+    'nil
+  )
+)
+
+; (LAT? '(A B C))
+; (LAT? '())
+; (LAT? 'A)
+; (LAT? '(A (B C) D))
 
